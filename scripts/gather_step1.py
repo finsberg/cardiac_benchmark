75a791cf80c8a1bca1136469719c181f49851130
Henrik Nicolay Finsberg <henriknf@simula.no>
2023-06-15 08:46:26 +0200

    Update script for gathering the results

--- scripts/gather_step1.py
+++ scripts/gather_step1.py
@@ -7,47 +7,58 @@ import cardiac_benchmark
 here = Path(__file__).absolute().parent
 
 
-resultdir = Path("/global/D1/homes/henriknf/cardiac_benchmark/step1/491494")
-disp_path = here / ".." / "data" / "displacement_points.npz"
-vol_path = here / ".." / "data" / "computed_vols.npz"
+def main(jobid, label):
+    resultdir = Path(f"/global/D1/homes/henriknf/cardiac_benchmark/step1/{jobid}")
+    disp_path = here / ".." / "data" / "displacement_points.npz"
+    vol_path = here / ".." / "data" / "computed_vols.npz"
 
-loader = cardiac_benchmark.postprocess.DataLoader(resultdir / "result.h5")
+    loader = cardiac_benchmark.postprocess.DataLoader(resultdir / "result.h5")
 
-time = loader.time_stamps
-up0 = np.load(resultdir / "componentwise_displacement_up0.npy")
-up1 = np.load(resultdir / "componentwise_displacement_up1.npy")
-vol = np.load(resultdir / "volume.npy")
+    time = loader.time_stamps
+    up0 = np.load(resultdir / "componentwise_displacement_up0.npy")
+    up1 = np.load(resultdir / "componentwise_displacement_up1.npy")
+    vol = np.load(resultdir / "volume.npy")
 
-vol_true = cardiac_benchmark.postprocess.load_true_volume_data(vol_path=vol_path)
-disp_true = cardiac_benchmark.postprocess.load_true_displacement_data(
-    disp_path=disp_path,
-)
+    vol_true = cardiac_benchmark.postprocess.load_true_volume_data(vol_path=vol_path)
+    disp_true = cardiac_benchmark.postprocess.load_true_displacement_data(
+        disp_path=disp_path,
+    )
 
-outdir = Path("step1_no_pressure")
-outdir.mkdir(exist_ok=True)
+    outdir = Path(f"step1_{label}")
+    outdir.mkdir(exist_ok=True)
 
-cardiac_benchmark.postprocess.plot_componentwise_displacement_comparison(
-    up0,
-    up1,
-    time,
-    disp_true.up0,
-    disp_true.up1,
-    disp_true.time,
-    fname=outdir / "componentwise_displacement_comparison.png",
-)
+    cardiac_benchmark.postprocess.plot_componentwise_displacement_comparison(
+        up0,
+        up1,
+        time,
+        disp_true.up0,
+        disp_true.up1,
+        disp_true.time,
+        fname=outdir / "componentwise_displacement_comparison.png",
+    )
 
-cardiac_benchmark.postprocess.plot_volume_comparison(
-    vol,
-    time,
-    vol_true,
-    disp_true.time,
-    fname=outdir / "volume_comparison.png",
-)
+    cardiac_benchmark.postprocess.plot_volume_comparison(
+        vol,
+        time,
+        vol_true,
+        disp_true.time,
+        fname=outdir / "volume_comparison.png",
+    )
+
+    np.save(outdir / "up0.npy", up0)
+    np.save(outdir / "up1.npy", up1)
+    np.save(outdir / "time.npy", time)
+    np.save(outdir / "vol.npy", vol)
+    shutil.make_archive(base_name="step1", format="zip", root_dir=outdir)
+    shutil.copy("step1.zip", resultdir.parent / f"step1_{label}.zip")
 
 
-np.save(outdir / "up0.npy", up0)
-np.save(outdir / "up1.npy", up1)
-np.save(outdir / "time.npy", time)
-np.save(outdir / "vol.npy", vol)
-shutil.make_archive(base_name="step1", format="zip", root_dir=outdir)
-shutil.copy("step1.zip", resultdir.parent / "step1_P1.zip")
+if __name__ == "__main__":
+    data = {
+        491494: "P1",
+        491461: "zero_pressure_P1",
+        491630: "P2",
+        491631: "zero_pressure_P2",
+    }
+    for jobid, label in data.items():
+        main(jobid, label)
--- scripts/gather_step2.py
+++ scripts/gather_step2.py
@@ -16,92 +16,10 @@ case & $\mathrm{max}|u(p_0)|$ & $\mathrm{max}|u(p_1)|$ & $\mathrm{max}|\sigma(p_
 """
 
 
-def gather_zero_pressure():
-    features = {}
-    outdir = step2dir / "step2_zero_pressure_results_P1"
-    outdir.mkdir(exist_ok=True)
-    table = TABLE
-
-    for casedir in step2dir.iterdir():
-        if "case" not in casedir.stem:
-            continue
-        case_nr = int(casedir.stem.lstrip("case"))
-
-        for jobdir in casedir.iterdir():
-            print(jobdir)
-            parameter_file = jobdir / "parameters.json"
-            if not parameter_file.is_file():
-                continue
-
-            params = json.loads(parameter_file.read_text())
-
-            if params["alpha_m"] == params["alpha_m"] == 0:
-                # This using newmark beta method but we are using the
-                # generalized alpha method
-                continue
-
-            if params.get("pressure") != "none":
-                continue
-
-            if not (jobdir / "componentwise_displacement_up0.npy").is_file():
-                continue
-
-            print("Found results!")
-            # loader = cardiac_benchmark.postprocess.DataLoader(jobdir / "result.h5")
-
-            up0 = np.load(jobdir / "componentwise_displacement_up0.npy")
-            up1 = np.load(jobdir / "componentwise_displacement_up1.npy")
-
-            sp0 = np.load(jobdir / "von_Mises_stress_sp0.npy")
-            sp1 = np.load(jobdir / "von_Mises_stress_sp1.npy")
-            features[case_nr] = {
-                "max_up0": np.linalg.norm(up0, axis=1).max(),
-                "max_up1": np.linalg.norm(up1, axis=1).max(),
-                "max_sp0": np.abs(sp0).max(),
-                "max_sp1": np.abs(sp1).max(),
-            }
-
-            # cardiac_benchmark.postprocess.plot_componentwise_displacement(
-            #     up0=up0,
-            #     up1=up1,
-            #     time_stamps=loader.time_stamps,
-            #     fname=outdir / f"case_{case_nr}_componentwise_displacement.png",
-            # )
-            # cardiac_benchmark.postprocess.plot_von_Mises_stress(
-            #     sp0=sp0,
-            #     sp1=sp1,
-            #     time_stamps=loader.time_stamps,
-            #     fname=outdir / f"case_{case_nr}_von_Mises.png",
-            # )
-            # np.save(outdir / f"case_{case_nr}_time", loader.time_stamps)
-            # np.save(outdir / f"case_{case_nr}_u_p0", up0)
-            # np.save(outdir / f"case_{case_nr}_u_p1", up1)
-            # np.save(outdir / f"case_{case_nr}_vonMises_p0", sp0)
-            # np.save(outdir / f"case_{case_nr}_vonMises_p1", sp1)
-            # (outdir / f"case_{case_nr}_features.json").write_text(
-            #     json.dumps(features, indent=2)
-            # )
-
-            table += (
-                f"{case_nr} & "
-                + " & ".join(f"{v:.3f}" for v in features[case_nr].values())
-                + r"\\"
-                + "\n"
-            )
-
-    table += r"\end{tabular}"
-    (outdir / "features_zero_pressure.text").write_text(table)
-    (outdir / "all_features_zero_pressure.json").write_text(
-        json.dumps(features, indent=2),
-    )
-    shutil.make_archive(base_name="step2", format="zip", root_dir=outdir)
-    shutil.copy("step2.zip", step2dir / "step2_zero_pressure_P1.zip")
-
-
-def gather():
+def gather(pressure, function_space, label):
     table = TABLE
     features = {}
-    outdir = step2dir / "step2_results_P1"
+    outdir = step2dir / f"step2_{label}"
     outdir.mkdir(exist_ok=True)
 
     for casedir in step2dir.iterdir():
@@ -122,7 +40,10 @@ def gather():
                 # generalized alpha method
                 continue
 
-            if params.get("pressure") != "bestel":
+            if params.get("function_space", "P_1") != function_space:
+                continue
+
+            if params.get("pressure") != pressure:
                 continue
 
             if not (jobdir / "componentwise_displacement_up0.npy").is_file():
@@ -142,27 +63,6 @@ def gather():
                 "max_sp0": np.abs(sp0).max(),
                 "max_sp1": np.abs(sp1).max(),
             }
-
-            # cardiac_benchmark.postprocess.plot_componentwise_displacement(
-            #     up0=up0,
-            #     up1=up1,
-            #     time_stamps=loader.time_stamps,
-            #     fname=outdir / f"case_{case_nr}_componentwise_displacement.png",
-            # )
-            # cardiac_benchmark.postprocess.plot_von_Mises_stress(
-            #     sp0=sp0,
-            #     sp1=sp1,
-            #     time_stamps=loader.time_stamps,
-            #     fname=outdir / f"case_{case_nr}_von_Mises.png",
-            # )
-            # np.save(outdir / f"case_{case_nr}_time", loader.time_stamps)
-            # np.save(outdir / f"case_{case_nr}_u_p0", up0)
-            # np.save(outdir / f"case_{case_nr}_u_p1", up1)
-            # np.save(outdir / f"case_{case_nr}_vonMises_p0", sp0)
-            # np.save(outdir / f"case_{case_nr}_vonMises_p1", sp1)
-            # (outdir / f"case_{case_nr}_features.json").write_text(
-            #     json.dumps(features, indent=2)
-            # )
 
             table += (
                 f"{case_nr} & "
@@ -175,9 +75,20 @@ def gather():
     (outdir / "features.text").write_text(table)
     (outdir / "all_features.json").write_text(json.dumps(features, indent=2))
     shutil.make_archive(base_name="step2", format="zip", root_dir=outdir)
-    shutil.copy("step2.zip", step2dir / "step2_P1.zip")
+    shutil.copy("step2.zip", step2dir / f"step2_{label}.zip")
+
+
+def main():
+
+    data = [
+        ("bestel", "P_1", "P1"),
+        ("none", "P_1", "zero_pressure_P1"),
+        ("bestel", "P_2", "P2"),
+        ("none", "P_2", "zero_pressure_P2"),
+    ]
+    for pressure, function_space, label in data:
+        gather(pressure, function_space, label)
 
 
 if __name__ == "__main__":
-    gather()
-    gather_zero_pressure()
+    main()
