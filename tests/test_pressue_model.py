96da499635dae418fcb68a292e72f5199e1521ec
Henrik Finsberg <henrikfinsberg@hotmail.com>
2023-07-04 19:36:23 +0200

    Fix test for pressure model

--- tests/test_pressue_model.py
+++ tests/test_pressue_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from cardiac_benchmark.pressure_model import default_parameters_benchmark1
 from cardiac_benchmark.pressure_model import pressure_function
 from cardiac_benchmark.problem import LVProblem
 
@@ -9,7 +10,11 @@ def test_active_model_default_parameters():
     dt = float(problem_parameters["dt"])
     time = np.arange(dt, 1, dt)
     t_eval = time - float(problem_parameters["alpha_f"]) * dt
-    pressure = pressure_function(t_span=(0, 1), t_eval=t_eval)
+    pressure = pressure_function(
+        t_span=(0, 1),
+        t_eval=t_eval,
+        parameters=default_parameters_benchmark1(),
+    )
     # Different value than in the paper, but it depends on dt, alpha_f and integration method
     assert np.isclose(pressure.max(), 16073.142358747114)
 
